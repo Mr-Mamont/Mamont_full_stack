193f405c3b6cc9cf4a6b679eb303a4e83ea0d6f3
Mr-Mamont <ermamontov@gmail.com>
2022-09-22 18:35:38 +0300

    add Добавлен алгоритм решения для задачи

---
+++ itogovaya_rabota/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] Array1 = new string[6] {"123", "23", "hello", "world", "rus", "=+12(0"};
+string[] Array2 = new string[Array1.Length];
+void SecondArrayWithIF(string[] Array1, string[] Array2)
+{
+    int count = 0;
+    for (int i = 0; i < Array1.Length; i++)
+    {
+    if(Array1[i].Length <= 3)
+        {
+        Array2[count] = Array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{Array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(Array1, Array2);
+PrintArray(Array2);
